8c50f754d7c1692261a0046adf14c302119dcf7d
DianaBlah <dianablah2@gmail.com>
2023-11-14 10:23:05 +0300

    fixed lesson 4.7 v2

--- src/test/java/com/example/demo1/MainPageTest.java
+++ src/test/java/com/example/demo1/MainPageTest.java
@@ -39,29 +39,27 @@ public class MainPageTest {
     public void search() {
         String input = "Selenium";
         WebElement searchField = driver.findElement(By.cssSelector("#sb_form_q"));
+        By cite = By.cssSelector(":not(.b_adurl) > cite");
         searchField.sendKeys(input);
         searchField.submit();
 
-        //WebElement searchPageField = driver.findElement(By.cssSelector("#sb_form_q"));
-        //assertEquals(input, searchPageField.getAttribute("value"));
-
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(6));
         wait.until(ExpectedConditions.and(
-                ExpectedConditions.attributeContains(By.cssSelector("h2 > a[href]"), "href", "selenium"),
-                ExpectedConditions.elementToBeClickable(By.cssSelector("h2 > a[href]"))
-        ));
-        List<WebElement> results = driver.findElements(By.cssSelector(":not(.b_adurl) > cite"));
+                ExpectedConditions.textToBePresentInElementLocated(cite, "selenium"),
+                ExpectedConditions.elementToBeClickable(cite)));
+
+        List<WebElement> results = driver.findElements(cite);
         clickElement(results, 0);
         ArrayList<String> tabs = new ArrayList<> (driver.getWindowHandles());
         driver.switchTo().window(tabs.get(0));
 
-        System.out.println("Перешли по ссылке");
         String currentUrl = driver.getCurrentUrl();
-        assertEquals("https://www.selenium.dev/", currentUrl);
+        assertEquals("https://www.selenium.dev/", currentUrl, "Открылась другая ссылка");
     }
 
     public void clickElement(List<WebElement> results, int num){
         results.get(num).click();
+        System.out.println("Перешли по ссылке");
     }
 }
 
